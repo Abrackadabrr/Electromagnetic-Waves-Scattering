de3cba0e6c4e5ce087c196caa1a3aa6d1d706251
Abrackadabrr <iliynea2002@ya.ru>
2024-01-31 17:31:27 +0300

    Addition of gauss points

---
+++ src/integration/gauss_quadrature/GaussQuadrature.hpp
@@ -0,0 +1,102 @@
+//
+// Created by evgen on 31.01.24.
+//
+
+#ifndef ELECTROMAGNETIC_WAVES_SCATTERING_GAUSSQUADRATURE_HPP
+#define ELECTROMAGNETIC_WAVES_SCATTERING_GAUSSQUADRATURE_HPP
+
+#include "Types.hpp"
+
+
+#include <utility>
+#include <tuple>
+
+namespace SatelliteSystem::DefiniteIntegrals {
+
+    namespace detail {
+
+        template <typename Arg>
+        using DeltaType = decltype(std::declval<Arg>() - std::declval<Arg>());
+
+        template <typename FReturnType, typename... Args>
+        struct IntegralTypes {
+            using ArgsTuple = std::tuple<Args...>;
+            using DeltasTuple = std::tuple<DeltaType<Args>...>;
+            using MeasureType = decltype((std::declval<DeltaType<Args>>() * ...));
+            using ResultType = decltype(std::declval<MeasureType>() * std::declval<FReturnType>());
+        };
+
+        template <typename FReturnType, typename... Args>
+        IntegralTypes<FReturnType, Args...> extractIntegralTypes(const FReturnType& /*fReturnType*/,
+                                                                 const std::tuple<Args...>& /*args*/) {
+            return {};
+        }
+
+        template <typename Callable>
+        using ExtructedIntegralTypes =
+        decltype(extractIntegralTypes(std::declval<TypeTraits::return_type_of_t<Callable>>(),
+        std::declval<TypeTraits::argument_tuple_type_of_t<Callable>>()));
+
+        template <typename ArgsTuple, typename DeltaTuple, typename Points, indexType... indexes>
+        ArgsTuple createPoint(const ArgsTuple& start, const DeltaTuple& deltas, const Points& points,
+                              const std::index_sequence<indexes...>& /*sequence*/) {
+            return {std::get<indexes>(start)  + (std::get<indexes>(deltas) / 2) * (std::get<indexes>(points) + 1) ...};
+        }
+
+        template <typename DeltaTuple, indexType... indexes>
+        decltype(auto) calcArea(const DeltaTuple& deltas, const std::index_sequence<indexes...>& /*sequence*/) {
+            return (std::get<indexes>(deltas) * ...);
+        }
+
+        template <typename Quadrature, typename Callable, indexType... indexes>
+        typename ExtructedIntegralTypes<Callable>::ResultType calcQuadratureSum(
+                const Callable& f, const typename ExtructedIntegralTypes<Callable>::ArgsTuple& startArgs,
+                const typename ExtructedIntegralTypes<Callable>::DeltasTuple& deltas,
+                const std::index_sequence<indexes...>& /*sumSequence*/) {
+            constexpr auto dimSize = std::tuple_size_v<typename ExtructedIntegralTypes<Callable>::ArgsTuple>;
+            static_assert(Quadrature::dim == dimSize);
+            constexpr auto pointSequence = std::make_index_sequence<dimSize>();
+
+            return ((Quadrature::nodes[indexes].weight *
+                     std::apply(f, createPoint(startArgs, deltas, Quadrature::nodes[indexes].point, pointSequence))) +
+                           ...) *
+                   (calcArea(deltas, pointSequence) / std::pow(2, dimSize));
+        }
+
+    }  // namespace detail
+
+/** Выполняет интегрирование в многомерной прямоугольной области
+ *
+ * @tparam Quadrature       структура квадратуры. Содержит поля :
+ *                              dim - размерность квадратуры,
+ *                              size - количесто точек в квадратуре
+ *                              points - точки квадратуры
+ *                              weights - веса квадратуры
+ *
+ * @tparam Callable         Тип функция для интегрирования. Количество аргументов должно совпадать с dim в квадратуре
+ *
+ * @param f                 Объект функции для интегрирования
+ * @param startArgs         начальные точки для интегрирования (координаты нижней левой точки)
+ * @param deltas            длины ребер многомерного прямоугольника, в которой происходит интегрирование
+ *
+ * @return                  Значение интеграла
+ */
+    template <typename Quadrature, typename Callable>
+    typename detail::ExtructedIntegralTypes<Callable>::ResultType integrate(
+            const Callable& f, const typename detail::ExtructedIntegralTypes<Callable>::ArgsTuple& startArgs,
+            const typename detail::ExtructedIntegralTypes<Callable>::DeltasTuple& deltas) {
+        return detail::calcQuadratureSum<Quadrature>(f, startArgs, deltas, std::make_index_sequence<Quadrature::size>());
+    }
+
+//template <typename Quadrature, typename Callable>
+//typename detail::ExtructedIntegralTypes<Callable>::ResultType integrate(
+//    const Callable& f, const typename detail::ExtructedIntegralTypes<Callable>::ArgsTuple& startArgs,
+//    const typename detail::ExtructedIntegralTypes<Callable>::ArgsTuple& endArgs,
+//    const typename detail::ExtructedIntegralTypes<Callable>::DeltasTuple& steps) {
+//
+//    return detail::calcQuadratureSum<Quadrature>(f, startArgs, deltas, std::make_index_sequence<Quadrature::size>());
+//}
+
+}
+
+#endif //ELECTROMAGNETIC_WAVES_SCATTERING_GAUSSQUADRATURE_HPP
---
+++ tests/integration/plot_gauss_point.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.polynomial import legendre as leg
+
+nx = 7
+ny = 8
+x = leg.legroots([0] * nx + [1])
+y = leg.legroots([0] * ny + [1])
+x_grid, y_grid = np.meshgrid(x, y)
+print(x_grid)
+fig, ax = plt.subplots()
+ax.scatter(x_grid, y_grid, color="red")
+ax.grid()
+ax.plot([-1] * 2, [-1, 1], color="blue")
+ax.plot([1] * 2, [-1, 1], color="blue")
+ax.plot([-1, 1], [-1] * 2, color="blue")
+ax.plot([-1, 1], [1] * 2, color="blue")
+ax.minorticks_on()
+ax.set_aspect("equal")
+plt.show()
