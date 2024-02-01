4ba0eb79c931c1d590a172f420f9bbc290051cc8
Abrackadabrr <iliynea2002@ya.ru>
2024-02-01 14:35:34 +0300

    Task for integration created

--- examples/integration/task_integration.cpp
+++ examples/integration/task_integration.cpp
@@ -10,7 +10,7 @@ using EMW::Types::scalar;
 using namespace EMW;
 
 scalar f4(scalar x, scalar y) {
-    return x * x * y * y * y * y;;
+    return x * x * y * y * y * y;
 }
 
 scalar f5(scalar x, scalar y) {
@@ -21,9 +21,20 @@ scalar f(scalar x) {
     return (x + 1) * x * x * x * x;
 }
 
+struct functor {
+    int y;
+    explicit functor(int y_): y(y_) {};
+    scalar operator() (scalar x) const{
+        return (x + y) * x * x * x * x;
+    }
+};
+
 int main() {
     const scalar y = 1;
+    const functor func{1};
     auto f_l = [y] (scalar x) -> scalar {return (x + y) * x * x * x * x;};
-    const auto result = DefiniteIntegrals::integrate<DefiniteIntegrals::Quadrature<3>>(f_l, {0}, {2});
+    auto result = DefiniteIntegrals::integrate<DefiniteIntegrals::Quadrature<3>>(f_l, {0}, {2});
+    assert(std::abs(2 * 2 * 2 * 2 * 2 * 2. / 6 + 2 * 2 * 2 * 2 * 2. / 5 - result) < 1e-14);
+    result = DefiniteIntegrals::integrate<DefiniteIntegrals::Quadrature<3>>(func, {0}, {2});
     assert(std::abs(2 * 2 * 2 * 2 * 2 * 2. / 6 + 2 * 2 * 2 * 2 * 2. / 5 - result) < 1e-14);
 }
