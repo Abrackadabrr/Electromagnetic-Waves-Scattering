12003488cc7621bc95d42a0b60bcc2443024f1b6
Abrackadabrr <iliynea2002@ya.ru>
2024-07-03 19:48:36 +0300

    Volume Mesh structure refactoring

--- src/mesh/VolumeMesh.hpp
+++ src/mesh/VolumeMesh.hpp
@@ -13,20 +13,9 @@ namespace EMW::Mesh {
     class VolumeMesh {
         Containers::vector<Node> nodes_;
         const Mesh::SurfaceMesh &surfaceMesh_;
-        std::string name_ = "volume_mesh";
-
-        Types::Vector3c
-        getFirstPartIntegral(const Point &point, Types::complex_d k);
-
-        Types::Vector3c
-        getZeroPartIntegral(const Point &point, Types::complex_d k);
-
-        Types::Vector3c
-        getRIntegral(const Point &point, Types::complex_d k);
+        std::string name_ = "default_volume_mesh_name";
 
     public:
-        VolumeMesh() = delete;
-
         VolumeMesh(const Mesh::SurfaceMesh &surfaceMesh, const Containers::vector<Node> &nodes) :
                 surfaceMesh_(surfaceMesh), nodes_(nodes) {};
 
